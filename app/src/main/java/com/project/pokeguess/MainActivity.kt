d9cf54cedae0fb777e4126baff4a9787757b6b72
Ahmed <amdmoussa10@gmail.com>
2023-11-14 14:24:09 +0100

    test c

--- app/src/main/java/com/project/pokeguess/MainActivity.kt
+++ app/src/main/java/com/project/pokeguess/MainActivity.kt
@@ -175,7 +175,7 @@ class MainActivity : AppCompatActivity() {
         try {
             val pInfo: PackageInfo = packageManager.getPackageInfo(packageName, 0)
             val versionName: String = pInfo.versionName
-            versionTextView.text = "Version: $versionName"
+            versionTextView.text = "UI version: $versionName"
         } catch (e: Exception) {
             e.printStackTrace()
         }
